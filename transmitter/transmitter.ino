747c30ea6617ffcc63d0f5359835a31d8c1afb02
Ankit Maurya <ankitmaurya2531@gmail.com>
2025-08-16 00:20:46 +0530

    Write a simple protocol to send a message

--- transmitter/transmitter.ino
+++ transmitter/transmitter.ino
@@ -1,13 +1,32 @@
 #include<SPI.h>
 #include<LoRa.h>
 
-int counter=0;
+struct PacketHeader{
+  byte destinationAddress;
+  byte sourceAddress;
+  byte messageType;
+  byte payloadLength;
+  byte sequenceNumber;
+  byte checksum;
+};
+
+// int counter=0;
+
+byte calculateChecksum(const char* payload,byte payloadSize){
+  byte sum=0;
+  for(int i=0;i<payloadSize;i++){
+    sum+=payload[i];
+  }
+  return sum;
+}
+
+byte messageCounter=0;
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
   while(!Serial);
-  Serial.println("LoRa Transmitter");
+  Serial.println("LoRa Protocol Transmitter");
   if(!LoRa.begin(866E6)){
     Serial.println("Starting LoRa failed!");
     while(1);
@@ -17,14 +36,29 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  Serial.print("Sending packet: ");
-  Serial.println(counter);
+  char payload[]="Hello, it's a test msg.";
+  byte payloadSize=sizeof(payload);
+
+  PacketHeader header;
+  header.destinationAddress=0x02;
+  header.sourceAddress=0x01;
+  header.messageType=0x01;
+  header.sequenceNumber=messageCounter;
+  header.payloadLength=payloadSize;
+  header.checksum=calculateChecksum(payload,payloadSize);
+
+  byte packetBuffer[sizeof(header)+payloadSize];
+  memcpy(packetBuffer,&header,sizeof(header));
+  memcpy(packetBuffer+sizeof(header),payload,payloadSize);
+
+  Serial.print("Sending packet: #");
+  Serial.println(messageCounter);
   
   LoRa.beginPacket();
-  LoRa.print("Hello! Packet #");
-  LoRa.print(counter);
+  LoRa.write(packetBuffer,sizeof(packetBuffer));
   LoRa.endPacket();
-  counter++;
+  Serial.println("Sent!");
+  messageCounter++;
 
   delay(5000);
 
